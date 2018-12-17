4e038f8cb4e62b5f16d213cc4d02052989ab2808
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2018-12-17 08:23:13 -0500

    20181217 functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,14 +38,18 @@ var ranNum; // initiate the variable we will use to store the random number
 var answer; // initiate the holder for the transformer name
 var answerArray = []; // initiate the holder array for exploding the name
 var gameWon; // we'll use this later to see if all the right answers have been given and end the game. it's starting as undefined on purpose
+// ^^ did I ever use this?
 var wins = 0; // count of wins
-var losses; // count of losses
+var losses = 0; // count of losses
 var printAnswer = ""; // we'll use this to house to real-time output
 var guess; // create the "guess" var that will hold the current guess
+// ^^ did I ever use this?
 var badGuess = []; // create the array that will hold the bad guesses
 var guessesAllowed = 6; // we'll give the user six wrong guesses
 var guessesLeft = guessesAllowed; // this will be our active variable for the count as it decreases
 var gameArray = []; // create a new array that will be two dimension and include the answer + underscores
+var lastAnswer; // will hold the previous answer so we can keep it in memory for the alert
+// ^^ many eliminate this later
 
 function curAnswer() { // create function that can be called at any time to update the screen with the current contents of the secondary dimension of gameArray
     printAnswer = "";
@@ -58,6 +62,8 @@ function curAnswer() { // create function that can be called at any time to upda
 function updateStats() { // function to update remaining guesses count, and update incorrectly guessed letters
     document.getElementById("remaining").innerHTML = guessesLeft; // update guesses left
     document.getElementById("badguess").innerHTML = badGuess.join(", ");
+    document.getElementById("losses").innerHTML = losses; // update # of losses
+    document.getElementById("wins").innerHTML = wins; // update # of wins
 }
 
 // All the stuff needed to get a round started:
@@ -83,9 +89,9 @@ function newRound() {
     updateStats(); // update remaining guesses and incorrect guess list
 }
 
-newRound();
-
-
+newRound(); // trigger once at the beginning to start the game
+curAnswer(); // trigger once at the beginning to start the game
+updateStats(); // trigger once at the beginning to start the game
 
 // logging ------------------- logging 
 console.log(min); 
@@ -93,36 +99,17 @@ console.log(max);
 console.log(ranNum); 
 console.log(answer);
 console.log(answerArray);
-// logging ------------------- logging
-
-
-
-
-curAnswer();
-
-
-
-updateStats();
-
-// logging ------------------- logging
 console.log(gameArray);
-// logging ------------------- logging
+// logging ------------------- logging 
 
-
-
-// logging ------------------- logging
-// if (gameArray[0].includes("M")) { console.log("Included"); }
-// logging ------------------- logging
-
+// look for the key press
 document.onkeyup = function(event) {
 	var keyPressed = event.key.toUpperCase();
 	console.log("Key Press: " + keyPressed);
     if(badGuess.includes(keyPressed)){ // if the letter selected has already been picked once and is not part of the answer
         console.log("Already in badGuess");
         // stop. do nothing.
-    } /* else if ( 0 = 1 ) {
-// need to check against correct answers as well
-    } */ else {
+    } else {
         // loop through the gameArray to look for a match
         for (i=0;i<answerArray.length;i++) {
             if(keyPressed === gameArray[i][1]) { // if the letter selected is already in the second dimension
@@ -150,31 +137,37 @@ document.onkeyup = function(event) {
                 gameWon = true;
             }
         }
+        // logging ------------------- logging 
         console.log(gameArray);
         console.log(guessesLeft);
         console.log(badGuess);
         console.log("Game won: " + gameWon);
-        
-        // something needs to check to make sure the 
+        // logging ------------------- logging 
     }
     updateStats(); // print # guesses and incorrect guesses to html
     if(guessesLeft <= 0){ // now we need the logic to end the game if you got too many wrong
         console.log("Game over. You lost.");
+        losses = losses + 1;
+        lastAnswer = answer;
+        document.getElementById("botName").innerHTML = answer;
+        newRound(); // reset for the next round
+        updateStats();
+        alert("Sorry, you guessed too many incorrect letters. The answer was \"" + lastAnswer + "\".");
     }
     if(gameWon === true) { // logic to end the game if you get them all right
         console.log("You won! Great job!");
         wins = wins + 1;
-        document.getElementById("wins").innerHTML = wins; 
+        lastAnswer = answer;
+        document.getElementById("wins").innerHTML = wins; // ??? is this redundant?
         document.getElementById("botName").innerHTML = answer;
         // document.getElementById("botFaction").innerHTML = 
         // document.getElementById("botImage").innerHTML = 
         // document.getElementById("botQuote").innerHTML = 
         newRound(); // reset for the next round
         updateStats(); 
+        alert("Congrats! You correctly guessed \"" + lastAnswer +"\"!");
     }
-    
-    
-    // needs to be some sort of command to update some data on the screen
-}
+
+} // close event watching
 
 } // close onload
